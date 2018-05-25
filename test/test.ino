189aee8c920d2631c4797da75943d0e5d6b3ff9a
JordanFan860406 <j860406g@gmail.com>
2018-05-25 22:28:59 +0800

    traffic jam update

--- test/test.ino
+++ test/test.ino
@@ -1,13 +1,20 @@
 void setup() {
   // put your setup code here, to run once:
   //github
+  pinMode(1, OUTPUT);
   pinMode(13, OUTPUT);
+  pinMode(3, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(13, HIGH);
-  delay(1000);
+  delay(10000);
   digitalWrite(13, LOW);
-  delay(500);
+  digitalWrite(3, HIGH);
+  delay(1000);
+  digitalWrite(3, LOW);
+  digitalWrite(1, HIGH);
+  delay(5000);
+  digitalWrite(1, LOW);
 }
