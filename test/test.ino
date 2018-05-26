099f9a9c501d6f60c8ce04922abb1c9e5a223f19
JordanFan860406 <j860406g@gmail.com>
2018-05-26 20:40:45 +0800

    traffic jam update2

--- test/test.ino
+++ test/test.ino
@@ -11,10 +11,10 @@ void loop() {
   digitalWrite(13, HIGH);
   delay(10000);
   digitalWrite(13, LOW);
-  digitalWrite(3, HIGH);
-  delay(1000);
-  digitalWrite(3, LOW);
   digitalWrite(1, HIGH);
   delay(5000);
   digitalWrite(1, LOW);
+  digitalWrite(3, HIGH);
+  delay(1000);
+  digitalWrite(3, LOW);
 }
